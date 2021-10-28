1f38ff48dae3ec63c09c139e2126a7c50d1fb7d0
dvdprz <51711570+dvdprz@users.noreply.github.com>
2021-10-27 21:39:21 -0500

    testing flexbox

--- scripts/mainFunctions.js
+++ scripts/mainFunctions.js
@@ -1,11 +1,12 @@
-window.onscroll = function () {myFunction()};
+window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
+var header = document.getElementById("styles/style.css/myHeader");
+var sticky = header.offsetTop;
 
-function myFunction () {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
